35b7ac1b89b08d07880a1c694b4cde1d80776eae
M4lchi17 <151964280+malchi17@users.noreply.github.com>
2025-01-27 21:23:13 +0100

    Actualizar import random.py

--- import random.py
+++ import random.py
@@ -9,5 +9,5 @@ def generar_contraseña(longitud=8):
 if __name__ == "__main__":
     longitud = int(input("Introduce la longitud de la contraseña: "))
     contraseña = generar_contraseña(10)
-    print(f"Tu nueva contraseña es: {8QAZ*/ñx90
+    print(f"Tu nueva contraseña es:
                                      
