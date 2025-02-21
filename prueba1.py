2c5a2a43c8d65d9c1376cbe4f579d018ece3f59c
M4lchi17 <151964280+malchi17@users.noreply.github.com>
2025-02-21 17:28:58 +0100

    Add files via upload

---
+++ prueba1.py
@@ -0,0 +1,39 @@
+import hashlib
+import json
+import time
+
+class Block:
+    def __init__(self, index, previous_hash, timestamp, data, hash):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.timestamp = timestamp
+        self.data = data
+        self.hash = hash
+
+def calculate_hash(index, previous_hash, timestamp, data):
+    value = str(index) + previous_hash + str(timestamp) + json.dumps(data)
+    return hashlib.sha256(value.encode('utf-8')).hexdigest()
+
+def create_genesis_block():
+    return Block(0, "0", int(time.time()), "Genesis Block", calculate_hash(0, "0", int(time.time()), "Genesis Block"))
+
+def create_new_block(previous_block, data):
+    index = previous_block.index + 1
+    timestamp = int(time.time())
+    hash = calculate_hash(index, previous_block.hash, timestamp, data)
+    return Block(index, previous_block.hash, timestamp, data, hash)
+
+# Crear la cadena de bloques
+blockchain = [create_genesis_block()]
+previous_block = blockchain[0]
+
+# Añadir bloques a la cadena
+num_of_blocks_to_add = 10
+
+for i in range(num_of_blocks_to_add):
+    data = f"Block {i+1} Data"
+    new_block = create_new_block(previous_block, data)
+    blockchain.append(new_block)
+    previous_block = new_block
+    print(f"Block #{new_block.index} has been added to the blockchain!")
+    print(f"Hash: {new_block.hash}\n")
